bb98176dc83aae1d30341d42cb06a35969c78f02
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-09-21 19:22:08 -0300

    added comments

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,13 @@
 const menu = document.querySelector('[data-menu="btn"]');
 const lista = document.querySelector('[data-menu="lista"]');
 
+// Mostrar nav ao clicar no botão 'menu'
 function displayNav() {
   if (event.type === 'touchstart') event.preventDefault();
   lista.classList.toggle('active');
 }
 
+// Esconder a navegação pelo menu caso a tela fique maior que 769px
 function hideNav() {
   if (window.outerWidth >= 769) {
     lista.classList.remove('active');
--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -1,5 +1,6 @@
 const nav = document.querySelector('[data-nav]');
 
+// Adicionar classe 'sticky' quando a nav atingir o topo da tela
 function stickyNavBar() {
   if (window.pageYOffset >= nav.offsetTop) {
     nav.classList.add('sticky');
